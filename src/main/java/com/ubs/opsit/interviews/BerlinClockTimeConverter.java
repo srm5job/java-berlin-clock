edb3d52e4320db563d5d4db3a1a32cde8c7e6104
sandeep.r.m <sandy4000m@users.noreply.github.com>
2018-05-27 16:47:45 +0530

    Berlin Clock Time Converter Implementation

---
+++ src/main/java/com/ubs/opsit/interviews/BerlinClockTimeConverter.java
@@ -0,0 +1,11 @@
+package com.ubs.opsit.interviews;
+
+public class BerlinClockTimeConverter implements TimeConverter{
+	
+	@Override
+	public String convertTime(String aTime) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
