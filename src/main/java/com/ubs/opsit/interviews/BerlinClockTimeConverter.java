8eece57e6b8acdc29cb752d42ab6b7f42dfb8fe1
sandeep.r.m <sandy4000m@users.noreply.github.com>
2018-05-27 17:05:35 +0530

    Initial
    Initial

--- src/main/java/com/ubs/opsit/interviews/BerlinClockTimeConverter.java
+++ src/main/java/com/ubs/opsit/interviews/BerlinClockTimeConverter.java
@@ -5,7 +5,11 @@ public class BerlinClockTimeConverter implements TimeConverter{
 	@Override
 	public String convertTime(String aTime) {
 		// TODO Auto-generated method stub
-		return null;
+		return "O\r\n" + 
+				"RROO\r\n" + 
+				"RRRO\r\n" + 
+				"YYROOOOOOOO\r\n" + 
+				"YYOO";
 	}
 
 }
---
+++ src/test/java/com/ubs/opsit/interviews/BerlinClockTimeConverterTest.java
@@ -0,0 +1,34 @@
+package com.ubs.opsit.interviews;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BerlinClockTimeConverterTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
